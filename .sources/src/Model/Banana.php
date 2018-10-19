d696ce13049238a01945fa0b132d80c2a3262e37
Thomas Tischner <tti@netzmarkt.de>
2018-10-19 11:10:10 +0200

    Compatible repo for prophet

--- .sources/src/Model/Banana.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    namespace BananaService\Model;
-
-    class Banana extends \BananaService\Essentials\Model\Banana {
-        // This file has been automatically decoupled by prophet. Do not edit this.
-    }
--- .sources/src/Model/Exportable.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    namespace BananaService\Model;
-
-    interface Exportable extends \BananaService\Essentials\Model\Exportable {
-        // This file has been automatically decoupled by prophet. Do not edit this.
-    }
